b985f0d6d60d9b4b8b621988818726134bab1cc6
audibleblink <domain@me.com>
2021-08-14 09:12:31 -0400

    WIP

--- main.go
+++ main.go
@@ -9,9 +9,12 @@ import (
 )
 
 const (
+	SizeofListEntry      = 0x20
+	ListEntryOffset      = 0x20
 	PROCESS_ALL_ACCESS   = 0x1F0FFF
 	SE_PRIVILEGE_ENABLED = 0x00000002
-	PID                  = 0
+	PID                  = 8408
+	// PID = 0
 )
 
 func main() {
@@ -20,7 +23,6 @@ func main() {
 		fmt.Printf("seDebug: %s\n", err)
 		os.Exit(1)
 	}
-	// time.Sleep(90 * time.Second)
 
 	pidHandle, err := handleForPid(PID)
 	if err != nil {
@@ -28,64 +30,106 @@ func main() {
 		os.Exit(1)
 	}
 
-	pbi, err := getProcessBasicInformation(pidHandle)
+	pbi, err := procBasicInfo(pidHandle)
 	if err != nil {
 		fmt.Printf("getPBI: %s\n", err)
 		os.Exit(1)
 	}
 
-	ldr := pbi.PebBaseAddress.Ldr
-	for cur := ldr.InMemoryOrderModuleList.Flink; cur != &ldr.InMemoryOrderModuleList; cur = cur.Flink {
-		curP := unsafe.Pointer(cur)
-		structOffset := unsafe.Offsetof(windows.LDR_DATA_TABLE_ENTRY{}.InMemoryOrderLinks)
-		entry := (*windows.LDR_DATA_TABLE_ENTRY)(unsafe.Pointer(uintptr(curP) - structOffset))
-		fmt.Printf("%s\n", entry.FullDllName.String())
+	// partially fill the peb to get access to the Ldr.InMemoryOrderModuleList
+	err = fillRemotePEB(pidHandle, &pbi)
+	if err != nil {
+		fmt.Printf("readRemotePEB: %s\n", err)
+		os.Exit(1)
 	}
+
+	head := windows.LDR_DATA_TABLE_ENTRY{}
+	head.InMemoryOrderLinks.Flink = pbi.PebBaseAddress.Ldr.InMemoryOrderModuleList.Flink
+
+	stop := uintptr(unsafe.Pointer(pbi.PebBaseAddress.Ldr)) - ListEntryOffset
+
+	for uintptr(unsafe.Pointer(head.InMemoryOrderLinks.Flink)) != stop {
+		base := unsafe.Pointer(head.InMemoryOrderLinks.Flink)
+		size := uint32(unsafe.Sizeof(head))
+		dest := unsafe.Pointer(&head.InMemoryOrderLinks.Flink)
+		err = readMemory(pidHandle, base, dest, size)
+		if err != nil {
+			fmt.Printf("fart: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%#v\n", head.FullDllName.String())
+	}
+
 }
 
 func handleForPid(pid int) (handle windows.Handle, err error) {
 	if pid == 0 {
-		return windows.CurrentProcess(), err
+		handle = windows.CurrentProcess()
+		return
 	}
-	// attrs := PROCESS_ALL_ACCESS
-	attrs := windows.PROCESS_QUERY_INFORMATION | windows.PROCESS_VM_READ
-	handle, err = windows.OpenProcess(uint32(attrs), false, uint32(pid))
+	// attrs := windows.PROCESS_QUERY_INFORMATION | windows.PROCESS_VM_READ
+	attrs := PROCESS_ALL_ACCESS
+	handle, err = windows.OpenProcess(uint32(attrs), true, uint32(pid))
 	return
 }
 
-func getProcessBasicInformation(processHandle windows.Handle) (pbi windows.PROCESS_BASIC_INFORMATION, err error) {
-	pbiLen := uint32(unsafe.Sizeof(pbi))
-	err = windows.NtQueryInformationProcess(processHandle, windows.ProcessBasicInformation, unsafe.Pointer(&pbi), pbiLen, &pbiLen)
-	if err != nil {
-		return
-	}
+func procBasicInfo(handle windows.Handle) (pbi windows.PROCESS_BASIC_INFORMATION, err error) {
+	pbiSize := unsafe.Sizeof(windows.PROCESS_BASIC_INFORMATION{})
+	var returnedLen uint32
 
-	pbiStructLen := unsafe.Sizeof(windows.PROCESS_BASIC_INFORMATION{})
-	if pbiLen < uint32(pbiStructLen) {
-		err = fmt.Errorf("Bad size for process_basic_information")
-		return
-	}
+	err = windows.NtQueryInformationProcess(
+		handle,
+		windows.ProcessBasicInformation,
+		unsafe.Pointer(&pbi),
+		uint32(pbiSize),
+		&returnedLen)
 	return
 }
 
-func sePrivEnable(privString string) (err error) {
-	procHandle := windows.CurrentProcess()
+func fillRemotePEB(hProc windows.Handle, pbi *windows.PROCESS_BASIC_INFORMATION) error {
 
-	var tokenHandle windows.Token
-	windows.OpenProcessToken(procHandle, windows.TOKEN_ADJUST_PRIVILEGES, &tokenHandle)
-
-	var luid windows.LUID
-	err = windows.LookupPrivilegeValue(nil, windows.StringToUTF16Ptr(privString), &luid)
+	// read in top level peb
+	base := unsafe.Pointer(pbi.PebBaseAddress)
+	pbi.PebBaseAddress = &windows.PEB{}
+	size := uint32(unsafe.Sizeof(*pbi.PebBaseAddress))
+	dest := unsafe.Pointer(pbi.PebBaseAddress)
+	err := readMemory(hProc, base, dest, size)
 	if err != nil {
 		return err
 	}
 
-	privs := &windows.Tokenprivileges{}
-	privs.PrivilegeCount = 1
-	privs.Privileges[0].Luid = luid
-	privs.Privileges[0].Attributes = uint32(SE_PRIVILEGE_ENABLED)
+	// with peb.Ldr populated with the remote Ldr pointer, re-read
+	base = unsafe.Pointer(pbi.PebBaseAddress.Ldr)
+	pbi.PebBaseAddress.Ldr = &windows.PEB_LDR_DATA{}
+	size = uint32(unsafe.Sizeof(*pbi.PebBaseAddress.Ldr))
+	dest = unsafe.Pointer(pbi.PebBaseAddress.Ldr)
+	err = readMemory(hProc, base, dest, size)
+	if err != nil {
+		return err
+	}
 
-	returnLen := uint32(0)
-	err = windows.AdjustTokenPrivileges(tokenHandle, false, privs, 0, nil, &returnLen)
+	// base = unsafe.Pointer(&pbi.PebBaseAddress.Ldr.InMemoryOrderModuleList)
+	// pbi.PebBaseAddress.Ldr.InMemoryOrderModuleList = windows.LIST_ENTRY{}
+	// dest = unsafe.Pointer(&pbi.PebBaseAddress.Ldr.InMemoryOrderModuleList)
+	// size = uint32(unsafe.Sizeof(pbi.PebBaseAddress.Ldr.InMemoryOrderModuleList))
+	// err = readMemory(hProc, base, dest, size)
 	return err
+}
+
+func readMemory(hProc windows.Handle, start unsafe.Pointer, dest unsafe.Pointer, readLen uint32) error {
+	var bytesRead uint32
+	procNtReadVirtualMemory := windows.NewLazySystemDLL("ntdll.dll").NewProc("NtReadVirtualMemory")
+	ret, _, _ := procNtReadVirtualMemory.Call(
+		uintptr(hProc),                      // hProcess
+		uintptr(start),                      // start address
+		uintptr(dest),                       // destBuffer
+		uintptr(readLen),                    // bytes to read
+		uintptr(unsafe.Pointer(&bytesRead)), // post-read count
+	)
+
+	code := (windows.NTStatus)(uint32(ret))
+	if ret != 0 {
+		return fmt.Errorf("readProcessMemory: %s", code.Errno().Error())
+	}
+	return nil
 }
