3dcb726f022461fe36459ebf69e845a729d584ad
Alex Flores <domain@me.com>
2022-03-03 20:36:05 -0500

    update to use dllinquent

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/audibleblink/rpcls
 
-go 1.16
+go 1.18
 
 require (
-	github.com/Binject/debug v0.0.0-20210312092933-6277045c2fdf
+	github.com/Binject/debug v0.0.0-20211007083345-9605c99179ee
+	github.com/audibleblink/dllinquent v0.2.3
 	github.com/audibleblink/getsystem v0.1.1
-	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55
+	github.com/audibleblink/memutils v0.0.2
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9
 )
--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"unsafe"
 
 	"golang.org/x/sys/windows"
 
+	"github.com/audibleblink/dllinquent"
 	"github.com/audibleblink/getsystem"
-	"github.com/audibleblink/rpcls/pkg/memutils"
-	"github.com/audibleblink/rpcls/pkg/procs"
+	"github.com/audibleblink/memutils"
 )
 
 const (
-	RPCRT4DLL  = `C:\WINDOWS\System32\RPCRT4.dll`
+	RPCRT4DLL  = `RPCRT4.dll`
 	serverRole = "RpcServerListen"
 	clientRole = "RpcStringBindingCompose"
 )
@@ -55,7 +54,7 @@ func init() {
 func main() {
 
 	if doSystem {
-		pid := procs.PidForName("winlogon.exe")
+		pid := memutils.PidForName("winlogon.exe")
 		err := getsystem.InNewProcess(pid, "cmd.exe", false)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "You sure you're admin?")
@@ -63,118 +62,57 @@ func main() {
 		os.Exit(0)
 	}
 
-	processes, err := procs.Processes()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	processes, err := memutils.Processes()
+	bail(err, "failed to list procs")
 
 	getsystem.DebugPriv()
 
 	for _, proc := range processes {
-		prvs := windows.PROCESS_QUERY_INFORMATION | windows.PROCESS_VM_READ
-		pidHandle, err := memutils.HandleForPid(proc.Pid, prvs)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
 
-		user, err := getsystem.TokenOwnerFromPid(proc.Pid)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s (%d) | %s\n", proc.Exe, proc.Pid, err)
-			continue
-		}
+		walker, err := dllinquent.NewPebWalker(proc.Pid)
+		bail(err, "couldn't read peb for pid %d | %s", proc.Pid, err)
 
-		peb, err := memutils.GetPEB(pidHandle)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s (%d) | %s\n", proc.Exe, proc.Pid, err)
-			continue
-		}
+		var pe dllinquent.Dll
+		for walker.Walk() {
 
-		head := windows.LDR_DATA_TABLE_ENTRY{
-			InMemoryOrderLinks: peb.Ldr.InMemoryOrderModuleList,
-		}
+			dll := walker.Dll()
+			if pe == (dllinquent.Dll{}) {
+				pe = dll
+			}
 
-		var selfPESize uint64
-		firstRun := true
-		for {
-			// read the current LIST_ENTRY flink into a LDR_DATA_TABLE_ENTRY,
-			// inherently casting it
-			base := unsafe.Pointer(head.InMemoryOrderLinks.Flink)
-			size := uint32(unsafe.Sizeof(head))
-			dest := unsafe.Pointer(&head.InMemoryOrderLinks.Flink)
-			err = memutils.ReadMemory(pidHandle, base, dest, size)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "main | next_flink | %s\n", err)
+			if strings.ToLower(dll.DllBaseName) != strings.ToLower(RPCRT4DLL) {
 				continue
 			}
 
-			// populate the DLL Name buffer with the remote address currently
-			// stored at head.FullDllName
-			name, err := memutils.PopulateStrings(pidHandle, &head.FullDllName)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "main | pop_dll_name | %s\n", err)
+			peFile, err := memutils.CarveOutPE(walker.Handle, walker.PEB, pe.LdrDataTableEntry.SizeOfImage)
+			bail(err, "couldn't carve PE for pid %d | %s\n", proc.Pid, err)
+
+			imports, err := peFile.ImportedSymbols()
+			bail(err, "can't read imports (%d) | %s\n", proc.Pid, err)
+
+			role := getRole(imports)
+			if role == "" {
 				continue
 			}
 
-			// we're at the last dll in the linked-list
-			if name == "" {
-				break
+			user, err := getsystem.TokenOwnerFromPid(proc.Pid)
+			bail(err, "%s (%d) | %s\n", proc.Exe, proc.Pid, err)
+
+			result := Result{
+				Pid:  proc.Pid,
+				Name: proc.Exe,
+				User: user,
+				Cmd:  walker.PEB.ProcessParameters.CommandLine.String(),
+				Path: walker.PEB.ProcessParameters.ImagePathName.String(),
+				Role: role,
 			}
 
-			// the first entry in the pe list is the hosting process
-			// we take SizeOfImage so we know how much to read in later
-			if firstRun {
-				// have to cast to this custom struct becuase the built-in
-				// sys/windows one doesn't export SizeOfImage
-				newLdr := (*PebLdrDataTableEntry64)(unsafe.Pointer(&head))
-				selfPESize = newLdr.SizeOfImage
-				firstRun = false
+			out, err := json.Marshal(result)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "jsonMarshal: %s\n", err)
+				continue
 			}
-
-			isMatch := name == RPCRT4DLL
-
-			if isMatch {
-				// fetch the strings located at the address indicated
-				// in the peb's ProcessParameters
-				params := peb.ProcessParameters
-				cmd, err := memutils.PopulateStrings(pidHandle, &params.CommandLine)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "can't read cmd | %s (%d) | %s\n", name, proc.Pid, err)
-				}
-				path, err := memutils.PopulateStrings(pidHandle, &params.ImagePathName)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "can't read proc path | %s (%d) | %s\n", name, proc.Pid, err)
-				}
-
-				// extract the process' PE from memory
-				peFile, err := memutils.CarveOutPE(pidHandle, peb, selfPESize)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "carveOutPE | %s (%d) | %s\n", path, proc.Pid, err)
-					break
-				}
-
-				imports, err := peFile.ImportedSymbols()
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "can't read imports | %s (%d) | %s\n", path, proc.Pid, err)
-					break
-				}
-
-				// is it a server, client, both, or neither
-				role := getRole(imports)
-				if role == "" {
-					break
-				}
-
-				result := Result{proc.Pid, proc.Exe, user, cmd, path, role}
-				out, err := json.Marshal(result)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "jsonMarshal: %s\n", err)
-					break
-				}
-				fmt.Println(string(out))
-				isMatch = false
-			}
+			fmt.Println(string(out))
 		}
 	}
 }
@@ -202,5 +140,12 @@ func getRole(imports []string) string {
 		return "SERVER"
 	} else {
 		return ""
+	}
+}
+
+func bail(err error, msg string, i ...interface{}) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, msg, i...)
+		os.Exit(1)
 	}
 }
